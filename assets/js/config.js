e6f082465b38e612b42b5164ccaeabd5501730fa
Dracava <DaphneSim@live.nl>
2025-01-08 14:26:36 +0100

    Update API configuration

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,5 +1,5 @@
 const process = {
   env: {
-    OPENAI_API_KEY: '' // Leave empty in source control
+    OPENAI_API_KEY: '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' // Add your API key here for development
   }
 }; 
